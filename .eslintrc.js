0a6d4c268000921f5605931c4aa8f1b1699ca41e
hyeon9782 <hyeon9782@gmail.com>
2023-09-02 18:32:50 +0900

    chore: next 기본 설정을 진행합니다.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     extends: [
       'eslint:recommended',
+      'plugin:@typescript-eslint/recommended'
     ],
     plugins: ['@typescript-eslint'],
     parser: '@typescript-eslint/parser',
