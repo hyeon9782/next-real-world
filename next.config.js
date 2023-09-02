af57ba5b70766bcfb4ef433548d238e5ed73e23e
hyeon9782 <hyeon9782@gmail.com>
2023-09-02 19:48:01 +0900

    chore: prettier & eslint 설정 변경 및 vanilla extract 환경 설정

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
+const withVanillaExtract = createVanillaExtractPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = withVanillaExtract(nextConfig);
