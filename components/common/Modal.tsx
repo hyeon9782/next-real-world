f0932123dbcb1de296fbaf38fc0d14eab42a53be
hyeon9782 <hyeon9782@gmail.com>
2023-09-05 23:00:52 +0900

    test: 노트북 WSL 테스트를 진행합니다.

--- components/common/Modal.tsx
+++ components/common/Modal.tsx
@@ -4,9 +4,7 @@ import React from 'react';
 const Modal = () => {
   return (
     <div className={modalBackground}>
-      <div className={modalBlock}>
-        Modal입니다. WSL 테스트를 진행합니다.
-      </div>
+      <div className={modalBlock}>Modal입니다. WSL 테스트를 진행합니다. 노트북 WSL 테스트를 진행합니다.</div>
     </div>
   );
 };
